5d3abe84017f055982a91eda302bceb2a82c10d5
Evowind <tadlysamy@gmail.com>
2025-03-12 16:19:52 +0100

    Inital Spring project

---
+++ src/main/java/com/elite/dockereye_007/Dockereye007Application.java
@@ -0,0 +1,13 @@
+package com.elite.dockereye_007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockereye007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dockereye007Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/elite/dockereye_007/Dockereye007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elite.dockereye_007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dockereye007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
